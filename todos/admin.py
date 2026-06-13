a200953bea586df3f62fff8055bb40eb4465c0fe
gigi <gigichakaberia@gmail.com>
2026-06-13 04:14:09 +0400

    another commit

--- todos/admin.py
+++ todos/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 from .models import Todo, Profile
+
+# Unregister the provided User admin
+admin.site.unregister(User)
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profile'
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
 
 @admin.register(Todo)
 class TodoAdmin(admin.ModelAdmin):
     list_display = ["title", "completed", "user", "created_at"]
     list_filter = ["completed", "created_at"]
     search_fields = ["title"]
-
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'image']
